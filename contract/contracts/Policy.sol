ce1f3a1ebea3c14a8ef9b21439629097aa2c3dc5
Sam <John.Smith.Ed@outlook.com>
2021-12-22 21:03:53 +0800

    update the constructor

--- contract/contracts/Policy.sol
+++ contract/contracts/Policy.sol
@@ -7,10 +7,35 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
-contract Policy is Ownable,IStaking{
+contract Policy is Ownable{
     using SafeERC20 for IERC20;
     using SafeMath for uint256 ;
-    address staking;
+    using Address for address;
+    
+    IStaking staking;
+    
+    uint256 pID;
+    
+    struct policy {
+        uint256 policyid;
+        uint256 rewardBalnce;
+        uint256 begin;
+        uint256 end;
+        address[] stakers;
+        mapping(address => uint256) stakerInBlock;
+    }
+    
+    
+    
+    constructor(IStaking _staking) {
+        //require(_staking.isContract(),"only staking is controller");
+        staking = _staking;
+    }
+    
+    
+    
+    
     
 }
