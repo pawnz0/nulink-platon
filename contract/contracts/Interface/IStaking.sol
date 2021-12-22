a93fb002650d9899be16e03079c0aa97e8e5b201
Sam <John.Smith.Ed@outlook.com>
2021-12-22 18:49:06 +0800

    add function and del policy struct

--- contract/contracts/Interface/IStaking.sol
+++ contract/contracts/Interface/IStaking.sol
@@ -3,6 +3,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 pragma solidity ^0.8.0;
 interface IStaking{
     
+    function isStaker(address _user) external view returns(bool);
     struct staker{
         address   owner;
         IERC20   stakeToken;
@@ -10,12 +11,5 @@ interface IStaking{
         uint256   workcount;
     }
 
-    struct policy {
-        uint256 policyid;
-        uint256 rewardBalnce;
-        uint256 begin;
-        uint256 end;
-        address[] stakers;
-        mapping(address => uint256) stakerInBlock;
-    }
+    
 }
