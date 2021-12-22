a718cc8676ba315e9b67ab2364f35e7e40c62f94
Sam <John.Smith.Ed@outlook.com>
2021-12-22 18:27:51 +0800

    update the staker struct

--- contract/contracts/Interface/IStaking.sol
+++ contract/contracts/Interface/IStaking.sol
@@ -1,17 +1,21 @@
 //SPDX-License-Identifier: UNLICENSED
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 pragma solidity ^0.8.0;
 interface IStaking{
+    
     struct staker{
         address   owner;
-        uint256   balance;
+        IERC20   stakeToken;
+        uint256   stakeBalance;
         uint256   workcount;
     }
 
     struct policy {
         uint256 policyid;
-        uint256 balance;
+        uint256 rewardBalnce;
         uint256 begin;
         uint256 end;
         address[] stakers;
+        mapping(address => uint256) stakerInBlock;
     }
 }
