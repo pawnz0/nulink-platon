dbf2030e597d86b5d81ebc444bf23461c569f0b1
relxss <relxgo.163.com>
2022-01-27 19:15:25 +0800

    modify policy contract

--- contract/contracts/Policy.sol
+++ contract/contracts/Policy.sol
@@ -9,177 +9,132 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
-contract Policy is Ownable{
+contract Policy is Ownable {
     using SafeERC20 for IERC20;
-    using SafeMath for uint256 ;
+    using SafeMath for uint256;
     using Address for address;
-    
+
     IStaking staking;
-    
-    uint256  public pID; //this is PID
+
+    uint256  public pID;
     IERC20  public rewardToken;
-    
+
     struct policy {
-        uint256 policyid;
+        uint256 policyID;
         address creater;
-        uint256 rewardBalnce; //
+        uint256 rewardBalance;
         uint256 begin;
         uint256 end;
         uint256 cancelBlock;
         address[] stakers;
     }
-    
-    //@policyIDList is user policy id list user can del  .
-    mapping(address=> uint256[])  public policyIDList; 
-    mapping(uint256 => policy) public policyInfo;
-    mapping(address => mapping(uint256 => uint256)) public stakerInBlock; // user => pid => inBlock
-    mapping(address => mapping(uint256 => uint256)) public stakerHavestBlock; // user => pid => inBlock
-    mapping(uint256 => address[]) public policyStakerList;
-    mapping(uint256 => mapping(address => bool)) public isPolicyStaker;
-    
-    
-    modifier onlyPolicyStaker(uint256 _pid){
-        require(isPolicyStaker[_pid][msg.sender],"only policy staker");
-        _;
-    }
-    
-    constructor(IStaking _staking,IERC20 _rewardToken) {
+
+    uint256[]  public policyIDList;
+    mapping(uint256 => policy) public policyInfo;  // policyID ==> policy
+    mapping(address => uint256) public stakerReward; // staker address ==> reward
+    mapping(uint256 => uint256) public policyRewardedBlock; // policyID ==> rewarded block
+
+    constructor(IStaking _staking, IERC20 _rewardToken) {
         //require(_staking.isContract(),"only staking is controller");
-        require(address(_rewardToken).isContract(),"only reward token is controller" );
+        require(address(_rewardToken).isContract(), "only reward token is controller");
         staking = _staking;
         rewardToken = _rewardToken;
     }
-    
-    /**
-     * @dev createPolicy any user can create ther policy 
-     */
-    function createPolicy(address[] memory _stakerAddress,uint256 _rewardBalance,uint256 _begin,uint256 _end) public {
-        //check staker 
-        require(checkStaker(_stakerAddress),"staker address not staker");
-        require(_end > _begin && _begin >= block.number,"create begin and end is illegal");
-        require(_rewardBalance > 0,"reward balance must big zero");
-        
-        pID+= pID;
-        
-        rewardToken.safeTransferFrom(msg.sender,address(this),_rewardBalance);
-        
-        policyInfo[pID] = policy({
-            policyid : pID,
-            creater : msg.sender,
-            rewardBalnce : _rewardBalance,
-            begin : _begin,
-            end : _end,
-            cancelBlock : 0,
-            stakers : _stakerAddress
-        });
-        
-        
-        for(uint256 i=0; i< _stakerAddress.length; i++){
-            policyStakerList[pID].push(_stakerAddress[i]);
-            isPolicyStaker[pID][_stakerAddress[i]] = true;
-        }
-        
-        
-        
-    }
-    
-    /**
-     * @dev cancelPolicy any user can cancel ther policy 
-     */
-    function cancelPolicy(uint256 _pid) public{
-        policy storage pl = policyInfo[_pid];
-        require(pl.creater == msg.sender,"this create not right");
-        require(pl.cancelBlock == 0,"alread canceled ");
-        uint256 endblock = block.number > pl.end ? pl.end : block.number;
-        
-        uint256 calcRewardBalance = calcReward(_pid);
-        
-        pl.cancelBlock = endblock;
-        
-        if(pl.rewardBalnce >= calcRewardBalance){
-            rewardToken.safeTransfer(msg.sender,pl.rewardBalnce.sub(calcRewardBalance));
-        }
-    }
 
-    
-    /**
-     * @dev joinPolicy staker need join policy can get reward 
-     */
-    function joinPolicy(uint256 _pid) public onlyPolicyStaker(_pid){
-        policy storage pl = policyInfo[_pid];
-        require(pl.cancelBlock == 0,"policy is cancel");
-        require(pl.creater != address(0),"policy not creater");
-        require(pl.end > block.number,"plocy is end");
-        
-        stakerInBlock[msg.sender][_pid] = block.number;
-        stakerHavestBlock[msg.sender][_pid] = block.number;
-    }
-    
-    /**
-     * @dev harvest staker can havest reward  
-     */
-    function harvest(uint256 _pid) public onlyPolicyStaker(_pid){
-        policy memory pl = policyInfo[_pid];
-        uint256 reward = calcStakerReward(_pid,msg.sender);
-        
-        require(reward <= pl.rewardBalnce,"reward calc error");
-        stakerHavestBlock[msg.sender][_pid] = block.number;
-        rewardToken.safeTransfer(msg.sender,reward);
-    }
-    
-    function checkStaker(address[] memory stakerAddress) view internal returns(bool){
-        for(uint256 i=0; i< stakerAddress.length; i++){
-            if(!staking.isStaker(stakerAddress[i])){
+    function checkStaker(address[] memory _stakerAddresses) view internal returns (bool){
+        for (uint256 i = 0; i < _stakerAddresses.length; i++) {
+            if (!staking.isStaker(_stakerAddresses[i])) {
                 return false;
             }
         }
         return true;
     }
-    
+
     /**
-     * @dev calcReward Calculate how much reward needs to be spent based on the current block  
-     */
-    function calcReward(uint256 _pid) public view returns(uint256 castReward) {
-        policy memory pl = policyInfo[_pid];
-        uint256 thisEndBlock = block.number;
-        if(pl.cancelBlock != 0){
+     * @dev createPolicy any user can create ther policy
+ */
+    function createPolicy(address[] memory _stakerAddresses, uint256 _rewardBalance, uint256 _begin, uint256 _end) public returns (uint256) {
+        //check staker
+        require(checkStaker(_stakerAddresses), "staker address not staker");
+        require(_end > _begin && _begin >= block.number, "create begin and end is illegal");
+        require(_rewardBalance > 0, "reward balance must big zero");
+
+        pID += 1;
+
+        rewardToken.safeTransferFrom(msg.sender, address(this), _rewardBalance);
+
+        policyInfo[pID] = policy({
+            policyID: pID,
+            creater: msg.sender,
+            rewardBalance: _rewardBalance,
+            begin: _begin,
+            end: _end,
+            cancelBlock: 0,
+            stakers: _stakerAddresses
+        });
+        policyIDList.push(pID);
+
+        return pID;
+    }
+
+    function cancelPolicy(uint256 _pid) public {
+        policy storage pl = policyInfo[_pid];
+        require(pl.creater == msg.sender, "this create not right");
+        require(pl.cancelBlock == 0, "already canceled ");
+        require(pl.end > block.number, "policy has expired");
+
+        pl.cancelBlock = block.number;
+        uint256 reward = calcReward(pl);
+        for (uint256 i = 0; i < pl.stakers.length; i++) {
+            stakerReward[pl.stakers[i]] += reward;
+        }
+        policyRewardedBlock[pl.policyID] = block.number;
+    }
+
+    function giveOutReward() public {
+        for (uint256 i = 0; i < policyIDList.length; i++) {
+            policy storage pl = policyInfo[policyIDList[i]];
+            uint256 reward = calcReward(pl);
+            if (reward > 0) {
+                for (uint256 j = 0; j < pl.stakers.length; j++) {
+                    stakerReward[pl.stakers[j]] += reward;
+                }
+                if (block.number >= pl.end) {
+                    pl.cancelBlock = pl.end;
+                    policyRewardedBlock[pl.policyID] = pl.end;
+                    return;
+                }
+                policyRewardedBlock[pl.policyID] = block.number;
+            }
+        }
+    }
+
+    function calcReward(policy memory _policy) private view returns (uint256) {
+        if (_policy.begin > block.number || _policy.cancelBlock != 0) {
             return 0;
         }
-        if(pl.rewardBalnce > 0){
-            if(pl.stakers.length > 0){
-                uint256 blockReward = pl.rewardBalnce.div(pl.end.sub(pl.begin)).div(pl.stakers.length);
-                
-                uint256 inBlock;
-                for(uint256 i=0; i< policyStakerList[_pid].length;i++){
-                    inBlock = stakerInBlock[policyStakerList[_pid][i]][_pid];
-                    
-                    if(thisEndBlock > inBlock && inBlock != 0){
-                        castReward = castReward.add(thisEndBlock.sub(inBlock).mul(blockReward));
-                    }
-                    
-                }
+
+        if (_policy.rewardBalance > 0 && _policy.stakers.length > 0) {
+            uint256 period;
+            uint256 rewarded = policyRewardedBlock[_policy.policyID];
+            uint256 begin = rewarded > 0 ? rewarded : _policy.begin;
+            if (begin > _policy.end) {
+                return 0;
             }
-           
-        }
-    }
-    
-    function calcStakerReward(uint256 _pid,address _staker) public view returns(uint256 reward) {
-        policy memory pl = policyInfo[_pid];
-        uint256 thisEndBlock = pl.cancelBlock != 0 ? pl.cancelBlock : block.number;
-        thisEndBlock = thisEndBlock > pl.end ? pl.end : thisEndBlock;
-        if(pl.rewardBalnce > 0){
-            if(pl.stakers.length > 0){
-                uint256 blockReward = pl.rewardBalnce.div(pl.end.sub(pl.begin)).div(pl.stakers.length);
-                uint256 inBlock = stakerHavestBlock[_staker][_pid] > stakerInBlock[_staker][_pid] ? stakerHavestBlock[_staker][_pid] : stakerInBlock[_staker][_pid];
-                
-                if(thisEndBlock > inBlock){
-                    reward = thisEndBlock.sub(inBlock).mul(blockReward);
-                }
-                 
+            if (block.number >= _policy.end) {
+                period = _policy.end.sub(begin);
+            } else {
+                period = block.number.sub(begin);
             }
-        
+            return period.mul(_policy.rewardBalance).div(_policy.end.sub(_policy.begin)).div(_policy.stakers.length);
         }
+        return 0;
     }
-    
-    
+
+    function claim(uint256 _amount) public {
+        require(stakerReward[msg.sender] >= _amount, "insufficient balance");
+
+        rewardToken.safeTransferFrom(address(this), msg.sender, _amount);
+        stakerReward[msg.sender] -= _amount;
+    }
 }
