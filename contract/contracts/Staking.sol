55fda82fd811ff311b07102e9f634974978ac4a7
relxss <relxgo.163.com>
2022-01-27 19:14:30 +0800

    modify staking contract

--- contract/contracts/Staking.sol
+++ contract/contracts/Staking.sol
@@ -7,27 +7,38 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract Staking is Ownable,IStaking{
-    
+contract Staking is Ownable, IStaking {
+
     using SafeERC20 for IERC20;
-    
+
     IERC20 sToken; // stake Token
 
-     //@stakeAmount to be staker need staking amount .
-    uint256 stakeAmount;
     uint256 public totalReward;
     uint256 public totalStakeBalance;
 
-    mapping(address => staker) public stakerInfo;
-    mapping(address => uint256) public stakerReward;
-    mapping(address => bool) public managers;
+    address[] public stakers;
+    mapping(address => staker) public stakerInfo;  // staker address ==> staker
+    mapping(address => uint256) public stakerReward;  // staker address ==> reward
+    mapping(address => bool) public managers;  // manager address ==> is manager
 
-    constructor (IERC20 _stakeToken, address _manager, uint256 _stakaAmount, uint256 _totalReward){
+    constructor (IERC20 _stakeToken, address _manager, uint256 _reward){
         sToken = _stakeToken;
         managers[msg.sender] = true;
         managers[_manager] = true;
-        stakeAmount = _stakaAmount;
-        totalReward = _totalReward;
+        if (_reward > 0) {
+            sToken.safeTransferFrom(msg.sender, address(this), _reward);
+            totalReward = _reward;
+        }
+    }
+
+//    receive() external payable {
+//        totalReward += msg.value;
+//    }
+
+    function addReward(uint256 _reward) public {
+        // require(_totalReward > 0, "total reward must be greater than 0");
+        sToken.safeTransferFrom(msg.sender, address(this), _reward);
+        totalReward += _reward;
     }
 
     modifier onlyManager() {
@@ -39,42 +50,16 @@ contract Staking is Ownable,IStaking{
         return managers[_sender];
     }
 
-    function setTotalReword(uint256 _totalReward) public onlyManager {
-        // require(_totalReward > 0, "total reward must be greater than 0");
-        totalReward = _totalReward;
-    }
-    
     /**
-     * @dev newStaker is user to be a new staker 
+     * @dev isStaker to check the staker
      */
-    function newStaker() public{
-        require(stakeAmount > 0, "stakeAmount amount not set");
-        staker memory lockstaker = stakerInfo[msg.sender];
-        require(lockstaker.owner == address(0),"is staker");
-        
-        // new staker 
-        lockstaker = staker({
-            owner : msg.sender,
-            stakeToken : sToken,
-            stakeBalance: stakeAmount,
-            workcount:0,
-            isWork: true
-        });
-        
-        stakerInfo[msg.sender] = lockstaker;
-        
-        //transfer
-        sToken.safeTransferFrom(msg.sender,address(this),stakeAmount);
+    function isStaker(address _user) public override view returns (bool){
+        return stakerInfo[_user].owner == _user ? true : false;
     }
 
-    /**
-    * @dev updateStaker
-     */
-    function updateStaker(address _owner, uint256 _balance, uint256 _workCount, bool _isWork) public {
+    function updateStaker(address _owner, uint256 _balance, uint256 _workCount, bool _isWork) private {
         require(_balance > 0, "stake balance not set");
 
-        bool isFirst = false;
-        uint256 reward;
         staker memory newStaker;
 
         newStaker = staker({
@@ -85,68 +70,45 @@ contract Staking is Ownable,IStaking{
             isWork: _isWork
         });
 
+        // staker not exits
+        if (stakerInfo[_owner].owner == address(0)) {
+            stakers.push(_owner);
+        }
         stakerInfo[_owner] = newStaker;
-        if (totalStakeBalance == 0) {
-            totalStakeBalance += _balance;
-            isFirst = true;
-        }
-
-        reward = calcReward(totalStakeBalance, _balance);
-        if (reward != 0) {
-            stakerReward[_owner] += reward;
-        }
-        if (!isFirst) {
-            totalStakeBalance += _balance;
-        }
+        totalStakeBalance += _balance;
     }
 
     function UpdateStakers(address[] memory _owners, uint256[] memory _balances, uint256[] memory _workCounts, bool[] memory _isWorks) public {
         require(_owners.length == _balances.length && _workCounts.length == _isWorks.length && _owners.length == _workCounts.length, "invalid parameter");
-        for (uint i = 0; i < _owners.length; i++) {
+        for (uint256 i = 0; i < _owners.length; i++) {
             updateStaker(_owners[i], _balances[i], _workCounts[i], _isWorks[i]);
         }
+        giveOutReward();
     }
-    
-    /**
-     * @dev leaveStaker is user to leave staker 
-     */
-    function leaveStaker() public{
-        staker storage lockstaker = stakerInfo[msg.sender];
-        require(isStaker(msg.sender),"is not staker");
-        
-        lockstaker.owner = address(0);
-        
-        //transfer
-        lockstaker.stakeToken.safeTransfer(msg.sender,lockstaker.stakeBalance);
-        
-    }
-    
-    /**
-     * @dev isStaker to check the staker  
-     */
-    function isStaker(address _user) public override view returns(bool){
-        return stakerInfo[_user].owner == _user? true:false;
-        
-    }
-    
-    
-    /**
-     * @dev setStakeInfo to update stake info stake token and stakeamount 
-     */
-    function updateStakeInfo(IERC20 _stakeToken,uint256 _stakaAmount)  public onlyOwner{
-        sToken = _stakeToken;
-        stakeAmount = _stakaAmount;
+
+    function giveOutReward() private {
+        for (uint256 i = 0; i < stakers.length; i++) {
+            if (totalReward <= 0) {
+                return;
+            }
+            staker memory s = stakerInfo[stakers[i]];
+            uint256 reward = calcReward(totalStakeBalance, s.stakeBalance);
+            if (reward > 0 && reward < totalReward) {
+                totalReward -= reward;
+                stakerReward[s.owner] += reward;
+            }
+        }
     }
 
     function calcReward(uint256 _totalStakeBalance, uint256 _stakeBalance) private view returns (uint256) {
         require(_totalStakeBalance > 0, "total stake balance is not set");
-        if (totalReward == 0 || _stakeBalance == 0 ) {
+        if (totalReward == 0 || _stakeBalance == 0) {
             return 0;
         }
         return totalReward * _stakeBalance / _totalStakeBalance;
     }
 
-    function claim(uint64 _amount) public {
+    function claim(uint256 _amount) public {
         require(stakerReward[msg.sender] >= _amount, "insufficient balance");
 
         sToken.safeTransferFrom(address(this), msg.sender, _amount);
